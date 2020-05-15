c2c8baac28cd320961c89108baadeb3ffa17b9f9
Borzenkov Andrey <bav999ms@ya.ru>
2020-05-15 08:50:21 +0300

    fix

--- codeforces/round594/D.cpp
+++ codeforces/round594/D.cpp
@@ -1,5 +1,6 @@
 #include <bits/stdc++.h>
 #define all(x) begin(x), end(x)
+#define data data_tmp
 
 using namespace std;
 using ll = long long;
@@ -11,6 +12,10 @@ ll a[N];
 ll pref[N];
 ll revpref[N];
 ll rev[int(1e6) + 1];
+bool eq[int(1e6) + 1];
+vector<int> data[N];
+int from[N];
+int to[N];
 
 ll fastPow(ll a, ll p) {
     ll ans = 1;
@@ -24,14 +29,12 @@ ll fastPow(ll a, ll p) {
     return ans;
 }
 
-struct SegmentTree { 
+struct SegmentTree {
     vector<vector<pair<int,ll>>> t;
     vector<vector<ll>> pref;
-    SegmentTree(int n, vector<vector<int>> data) : t(n * 4), pref(n * 4) {
+    SegmentTree(int n) : t(n * 4), pref(n * 4) {
         vector<pair<int, int>> arr;
         map<int,int> mp;
-        vector<int> from(n);
-        vector<int> to(n);
         for (int i = 0; i < n; i++) {
             from[i] = arr.size();
             for (int val : data[i]) {
@@ -51,21 +54,21 @@ struct SegmentTree {
         for (int i = 0; i < n; i++) {
             sort(build.begin() + from[i], build.begin() + to[i]);
         }
-        Build(0, 0, n, build, from, to);
+        Build(0, 0, n, build);
     }
 
-    void Build(int v, int tl, int tr, vector<pair<int,int>>& arr, vector<int>& from, vector<int>& to) {
+    void Build(int v, int tl, int tr, vector<pair<int,int>>& arr) {
         if (tl + 1 == tr) {
             t[v].reserve(to[tl] - from[tl]);
             for (int i = from[tl]; i < to[tl]; i++) {
                 t[v].push_back(arr[i]);
             }
         } else {
-            int tm = (tl + tr) / 2;
-            Build(v * 2 + 1, tl, tm, arr, from, to);
-            Build(v * 2 + 2, tm, tr, arr, from, to);
+            int tm = (tl + tr) >> 1;
+            Build(v * 2 + 1, tl, tm, arr);
+            Build(v * 2 + 2, tm, tr, arr);
             merge(all(t[v * 2 + 1]), all(t[v * 2 + 2]), back_inserter(t[v]));
-            while (t[v].back().first > tl) t[v].pop_back(); 
+            while (t[v].back().first > tl) t[v].pop_back();
             t[v].shrink_to_fit();
         }
         pref[v].resize(t[v].size());
@@ -76,7 +79,7 @@ struct SegmentTree {
             }
         }
     }
-    
+
     ll Get(int v, int tl, int tr, int l, int r) {
         if (tl >= r || tr <= l) return 1;
         if (tl >= l && tr <= r) {
@@ -96,11 +99,10 @@ struct SegmentTree {
             if (cur == -1) return 1;
             return pref[v][cur];
         } else {
-            int tm = (tl + tr) / 2;
+            int tm = (tl + tr) >> 1;
             return Get(v * 2 + 1, tl, tm, l, r) * Get(v * 2 + 2, tm, tr, l, r) % mod;
         }
     }
-
 };
 
 
@@ -122,9 +124,8 @@ int main() {
             revpref[i] = (revpref[i - 1] * revpref[i]) % mod;
         }
     }
-    
+
     vector<ll> primes;
-    vector<bool> eq(int(1e6 + 1));
     for (int i = 2; i <= 1e6; i++) {
         if (eq[i]) continue;
         primes.push_back(i);
@@ -133,17 +134,6 @@ int main() {
         }
     }
 
-    int q;
-    cin >> q;
-    vector<pair<int,int>> queries;
-    for (int i = 0; i < q; i++) {
-        int l, r;
-        cin >> l >> r;
-        l--, r--;
-        queries.push_back({l, r});
-    }
-    
-    vector<vector<int>> data(n);
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < primes.size(); j++) {
             if (primes[j] * primes[j] > a[i]) {
@@ -152,23 +142,23 @@ int main() {
             if (a[i] % primes[j]) continue;
             data[i].push_back(primes[j]);
             while (a[i] % primes[j] == 0) {
-                a[i] /= primes[j];    
+                a[i] /= primes[j];
             }
-        }    
+        }
         if (a[i] > 1) {
             data[i].push_back(a[i]);
         }
     }
-    SegmentTree tree(n, data);
-    
-    vector<ll> ans(q);
+
+    SegmentTree tree(n);
+    int q;
+    cin >> q;
     for (int i = 0; i < q; i++) {
-        auto& [l, r] = queries[i];
-        ans[i] = pref[r] * (l ? revpref[l - 1] : 1) % mod;
-        
+        int l, r;
+        cin >> l >> r;
+        l--, r--;
+        ll ans = pref[r] * (l ? revpref[l - 1] : 1) % mod;
         ll value = tree.Get(0, 0, n, l, r + 1);
-        ans[i] = (ans[i] * value) % mod;
-        cout << ans[i] << '\n';
+        cout << (ans * value) % mod << '\n';
     }
-    
 }
